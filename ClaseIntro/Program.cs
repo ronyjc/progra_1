658c475cde0b606d4153e27fdc25256d52a23bb1
ronyjc <ronyjcifuentes@gmail.com>
2024-10-17 17:14:46 -0600

    ClaseIntro

---
+++ ClaseIntro/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("Este programa realiza la suma de dos numeros");
+        Console.WriteLine("Por favor ingrese el primer numero: ");
+        int numero_a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Por favor ingrese el segundo numero: ");
+        int numero_b = int.Parse(Console.ReadLine());
+        int total = numero_a + numero_b;
+        Console.WriteLine("El resultado de la suma es: " + total);
+        
+
+
+
+    }
+}
