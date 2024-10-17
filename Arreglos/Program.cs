b767938640050a1940da70eb96ce427eb217ed3e
ronyjc <ronyjcifuentes@gmail.com>
2024-10-17 17:03:20 -0600

    Arreglos

---
+++ Arreglos/Program.cs
@@ -0,0 +1,37 @@
+﻿//Solicitar al usuario el ingreso de los datos
+Console.WriteLine("Por favor ingrese 10 numeros, uno a uno, presionando \"ENTER\" en cada ingreso");
+
+//Declarar el arreglo de datos
+double[] numeros = new double[10];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    numeros[i] = Convert.ToDouble(Console.ReadLine());
+
+}
+
+double total = 0;
+
+for (int i = 0; i < numeros.Length; i++) {
+
+    total = total + numeros[i];
+}
+
+double promedio = total / numeros.Length;
+Console.WriteLine("El promedio de los numeros es: "+promedio);
+Console.WriteLine("Los valores superiores al prmedio son: ");
+
+for (int i = 0; i < numeros.Length; i++) {
+
+    if (numeros[i] < promedio)
+    {
+        continue;
+
+    }
+
+    else {
+        Console.WriteLine(numeros[i]);
+    
+    }
+
+}
