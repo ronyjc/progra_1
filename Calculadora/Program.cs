08ac5411697eb982e4490bcea924f60a098fdb43
ronyjc <ronyjcifuentes@gmail.com>
2024-10-17 17:04:20 -0600

    Calculadora

---
+++ Calculadora/Program.cs
@@ -0,0 +1,37 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Bienvenido, por favor seleccione una operación \n 1. Suma \n 2. Resta \n 3. Multiplicación \n 4. División");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Por favor ingrese el primero numero");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Por favor ingrese el segundo numero");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+
+            switch (opcion)
+            {
+                case 1:
+                    double resultado = a + b;
+                    Console.WriteLine("El resultado es: " + resultado);
+                    break;
+                case 2:
+                    double resultado_2 = a - b;
+                    Console.WriteLine("El resultado es: " + resultado_2);
+                    break;
+                case 3:
+                    double resultado_3 = a * b;
+                    Console.WriteLine("El resultado es: " + resultado_3);
+                    break;
+                case 4:
+                    double resultado_4 = a / b;
+                    Console.WriteLine("El resultado es: " + resultado_4);
+                    break;
+            }
+        }
+    }
+}
