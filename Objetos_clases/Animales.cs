946131c59a7a5a93cb1e7638ba657d257333152e
ronyjc <ronyjcifuentes@gmail.com>
2024-10-24 16:55:20 -0600

    Ejemplo de objetos y clases

---
+++ Objetos_clases/Animales.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objetos_clases
+{
+    internal class Animales
+    {
+        string nombre;
+        string especie;
+        string clasificacion;
+        int edad;
+
+        public Animales(string nombre_in, string especie_in, string clasificacion_in, int edad_in)
+        { 
+            nombre = nombre_in;
+            especie = especie_in;
+            clasificacion = clasificacion_in;
+            edad = edad_in;
+        
+        }
+
+        public string getNombre() { 
+            return nombre;
+        }
+        public string getEspecie() { 
+            return especie;
+        }
+        public string getClasificacion() {
+            return clasificacion;
+        }
+        public int getEdad() {
+            return edad;
+        }
+
+
+    }
+}
---
+++ Objetos_clases/Program.cs
@@ -0,0 +1,19 @@
+﻿using Objetos_clases;
+
+Console.WriteLine("Vamos a crear un objeto, de la clase \"Animales\"");
+Console.WriteLine("Por favor ingrese el nombre del animal: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Ingrese la especie del animal: ");
+string especie = Console.ReadLine();
+Console.WriteLine("Ingrese la clasificación: ");
+string clasificacion = Console.ReadLine();
+Console.WriteLine("Ingrese la edad del animal");
+int edad = Convert.ToInt16(Console.ReadLine());
+
+Animales animal1 = new Animales(nombre, especie, clasificacion, edad);
+
+Console.WriteLine("El objeto creado es: ");
+Console.WriteLine(animal1.getNombre());
+Console.WriteLine(animal1.getEspecie());
+Console.WriteLine(animal1.getClasificacion());
+Console.WriteLine(animal1.getEdad());
