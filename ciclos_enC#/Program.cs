c11bd2c7c51fbbd6f8c082ec6a0cdfce5091a7c0
ronyjc <ronyjcifuentes@gmail.com>
2024-10-24 17:04:40 -0600

    Ejemplo de ciclos en C#

---
+++ ciclos_enC#/Program.cs
@@ -0,0 +1,24 @@
+﻿//
+
+Console.WriteLine("Por favor ingrese un numero");
+int valor = Convert.ToInt32(Console.ReadLine());
+
+int contador = valor;
+int cociente; int residuo;
+
+while (contador > 0) {
+
+   cociente = valor / contador;
+    residuo = valor % contador;
+
+    if ((cociente == 3)&&(residuo == 0))
+    {
+        Console.WriteLine("El divisor " + contador + " da como cociente el numero 3");   
+        break;
+    }
+    contador--;
+    if (contador == 1) Console.WriteLine("No se encontró ningun valor");
+}
+
+
+
