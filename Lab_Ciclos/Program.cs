d8aa4ec68717f1303e6998dd2886ce61a6c4be3e
ronyjc <ronyjcifuentes@gmail.com>
2024-10-24 16:50:15 -0600

    Laboratorio de ciclos While, For y otros

---
+++ Lab_Ciclos/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Por favor ingrese los nombres separados por espacio");
+
+string[] nombres = new string[100];
+
+string captura = Console.ReadLine();
+
+nombres = captura.Split(" ");
+
+for (int i = 0; i < nombres.Length; i++) { 
+    Console.WriteLine(nombres[i]);
+}
